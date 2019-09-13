63595d9d3132c30696b3da6e2f6292ac4271353f
tamannasiddiqui <tamannasiddiqui110@gmail.com>
2019-09-14 02:16:26 +0530

    work in progress for app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import Teams from "./components/Teams_nba";
+import Games from "./components/Games";
+import Typography from '@material-ui/core/Typography';
+import { Toolbar, Grid, withStyles, Tabs, Tab, Paper } from '@material-ui/core';
 
-function App() {
+
+const style = {
+  heading: {
+    width: "100%",
+    textAlign: "center",
+    marginTop: "5rem",
+    color: "#f16d32"
+  },
+  tabSelected: {
+    background: "#f16d32",
+    color: "white"
+  }
+};
+
+function App(props) {
+  const { classes } = props;
+  const [currentTab, setCurrentTab] = useState(0);
+  function handleTabChange(e, val) {
+    setCurrentTab(val);
+  }
   return (
-    <div>
-       
+    <div className="App">
+      <Toolbar>
+        <Grid container justify="center" className={classes.heading}>
+          <Typography variant="h3" color="inherit">
+            NBA REPO
+          </Typography>
+        </Grid>
+      </Toolbar>
+      <Grid container justify="center" spacing={40} style={{marginTop:"5rem"}}>
+        <Grid item xs={12} lg={9} md={10}>
+          <Paper>
+          <Tabs
+            value={currentTab}
+            onChange={handleTabChange}
+            variant="fullWidth"
+            centered
+            style={{ width: "100%" }}
+          >
+            <Tab label="NBA Teams" classes={{ selected: classes.tabSelected }} />
+            <Tab label="NBA Games" classes={{ selected: classes.tabSelected }} />
+          </Tabs>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} lg={9} md={10}>
+          {
+            currentTab === 0?<Teams></Teams>:<Games></Games>
+          }
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
-export default App;
+export default withStyles(style)(App);
