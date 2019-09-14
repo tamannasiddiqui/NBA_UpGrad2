a1b2117ae0b8d7278193badde688e67d0420e4dc
tamannasiddiqui <tamannasiddiqui110@gmail.com>
2019-09-14 23:35:23 +0530

    Styling , coding in process for the components

--- src/components/Games.js
+++ src/components/Games.js
@@ -13,7 +13,49 @@ function Loading() {
     )
 }
 
+function fetchData(page, callback) {
+    axios.get("https://www.balldontlie.io/api/v1/games?per_page=20&&page=" + page)
+        .then(res => callback(res.data.data));
+}
 
+function Games() {
+    const [data, setData] = useState(["Loading"]);
+    const [loading, setLoading] = useState(true);
+    let [modalOpen, setModalOpen] = useState(() => {
+        let temp = {};
+        for (let i=0; i<20; i++)
+            temp = {...temp, [i]: false};
+        return temp;
+    });
+    const [page, setPage] = useState(1);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    function handleNext(e) {
+        e.stopPropagation();
+        setPage((page+1));
+        setLoading(true);
+        fetchData(page, (data) => {setData(data); setLoading(false)});
+    }
+    function handlePrevious(e) {
+        e.stopPropagation();
+        if (page > 1) {
+            setPage((page-1));
+            setLoading(true);
+            fetchData(page, (data) => {setData(data); setLoading(false)});
+        }
+    }
+    function openModal(e,idx) {
+        e.stopPropagation();
+        console.log("openModal");
+        const temp = {...modalOpen, [idx]: true};
+        setModalOpen(temp);
+    }
+    function closeModal(idx) {
+        console.log("closeModal");
+        const temp = {...modalOpen, [idx]: false};
+        setModalOpen(temp);
+    }
+    if (data[0] === "Loading") 
+        fetchData(page, (data) => {setData(data); setLoading(false)});
     return (
         <Paper>
             <Grid container wrap="wrap" style={{ textAlign: "center" }} spacing={40}>
@@ -35,7 +77,14 @@ function Loading() {
                         })
                 }
             </Grid>
-            
+            <Grid container spacing={40} justify="center" style={{ textAlign: "center" }}>
+                <Grid item md={6} lg={6} xs={12}>
+                    <Button size="large" style={{ background: "#60cb5c", color: "white" }} onClick={handlePrevious}>Previous</Button>
+                </Grid>
+                <Grid item md={6} lg={6} xs={12}>
+                    <Button size="large" style={{ background: "#60cb5c", color: "white" }} onClick={handleNext}>Next</Button>
+                </Grid>
+            </Grid>
         </Paper>
     )
 }
